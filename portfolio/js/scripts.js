eb737f0d689aa0bdd115417a4f24de7b09ef515b
João Pedro Moreto Lourenção <jlourencao@alunos.utfpr.edu.br>
2023-10-27 11:49:44 -0300

    Add files via upload

---
+++ portfolio/js/scripts.js
@@ -0,0 +1,78 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+// Adicione este código ao seu arquivo script.js
+
+// Função para fazer o download do currículo
+function downloadResume() {
+    // Caminho relativo para o arquivo PDF do currículo no mesmo diretório do projeto
+    const resumePath = 'resume.pdf';
+
+    // Cria um elemento "a" (link)
+    const link = document.createElement('a');
+    link.href = resumePath;
+    link.target = '_blank'; // Abre em uma nova aba
+    link.download = 'resume_JoaoPedro.pdf'; // Define o nome do arquivo de download
+
+    // Clica no link automaticamente
+    link.click();
+}
+
+
+// Adiciona um ouvinte de evento ao botão de download
+const downloadButton = document.getElementById('downloadButton');
+downloadButton.addEventListener('click', downloadResume);
+
